d1c8b1e89518c19d2d42892b854425daf3a713ed
dataking <dataking@gmail.com>
2018-10-03 13:32:50 -0700

    fixed indents

--- srt-lookup.py
+++ srt-lookup.py
@@ -6,27 +6,30 @@ import argparse
 import pyscclient
 
 def handle_arguments():
-    parser = argparse/ArgumentParser(description="Check scan status of IP.")
-    parser.add_argument('--carkconf', '-c', dest=carkconf, \
-        help="Full path to the CyberARK config file.")
-    parser.add_argument('--ip', '-i', dest=ipaddr, \
-        help="IP address to lookup.")
-    args = parser.parse_args()
-    return args
+	parser = argparse.ArgumentParser(description="Check scan status of IP.")
+	parser.add_argument('--carkconf', '-c', dest='carkconf', \
+		help="Full path to the CyberARK config file.")
+	parser.add_argument('--ip', '-i', dest='ipaddr', \
+		help="IP address to lookup.")
+	args = parser.parse_args()
+	return args
 
 def main():
 	pp = pprint.PrettyPrinter(indent=4)
-	cark = 'cark_conf.json'
+	args = handle_arguments()
+	cark = args.carkconf
 	with open(cark, 'r') as f:
 		cark_conf = json.load(f)
 		passwd = pyscclient.Utils.get_cark_creds(cark_conf)
 	conn = pyscclient.Connection(cark_conf['schost'], cark_conf['username'], passwd)
-    # check the number of organizations
-    print("There are {0} organizations for this SC console.".format(len(conn.organizations)))
-    # loop through the list of organizations and check the restrictedIPs
-    # list any active scans
-    # check the ipList to see if the ip in question is a member
-    pass
+	# check the number of organizations
+	print("Checking {0} organization(s) on this SC console.".format(len(conn.organizations())))
+	# loop through the list of organizations and check the restrictedIPs
+	for org in conn.list_orgs():
+		pp.pprint(org.restrictedIPs)
+	# list any active scans
+	# check the ipList to see if the ip in question is a member
+	pass
 
 if __name__=='__main__':
-    main()
+	main()
