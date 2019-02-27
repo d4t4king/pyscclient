dcfcd25dca13392df214c65e60ad7113d3a1909c
dataking <dataking@gmail.com>
2019-02-27 13:14:09 -0800

    working towards the 'standard' package layout

---
+++ pyscclient/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='pyscclient',
+	version='0.1',
+	description='python classes to simplify working with the Tenable.SC API',
+	url='https://github.com/d4t4king/pyscclient',
+	author='Charles Heselton',
+	author_email='dataking@gmail.com',
+	license='GPL v3.0',
+	packages=['pyscclient'],
+	zip_safe=False)
+
