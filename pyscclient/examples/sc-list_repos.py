79d7b2fb2cb7970deab40483efca7cc30fdfee2f
dataking <dataking@gmail.com>
2019-03-01 13:41:31 -0800

    path tweaks and account tweaks

--- pyscclient/examples/sc-list_repos.py
+++ pyscclient/examples/sc-list_repos.py
@@ -4,12 +4,12 @@ import json
 import pprint
 from termcolor import cprint,colored
 import os, sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 import pyscclient
 
 def main():
 	pp = pprint.PrettyPrinter(indent=4)
-	cark = 'cark_conf.json'
+	cark = '/home/charlie/pyscclient-gh/pyscclient/cark_conf.json'
 	with open(cark, 'r') as f:
 		cark_conf = json.load(f)
 		passwd = pyscclient.Utils.get_cark_creds(cark_conf)
